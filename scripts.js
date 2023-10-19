489636f615a2c09df9cf0e34b952fdbccab751e3
Raulman08 <87127859+Raulman08@users.noreply.github.com>
2023-10-19 22:50:20 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -48,3 +48,18 @@ toggleButton.addEventListener("click", function() {
     toggleButton.innerHTML = "Activar modo claro";
   }
 });
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.documentElement.setAttribute('data-theme', "dark");
+} else {
+  document.documentElement.setAttribute('data-theme', "light");
+}
+window.matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', event => {
+    if (event.matches) {
+      //dark mode
+      document.documentElement.setAttribute('data-theme', "dark")
+    } else {
+      //light mode
+      document.documentElement.setAttribute('data-theme', "light");
+    }
+  })
